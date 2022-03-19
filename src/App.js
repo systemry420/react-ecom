7f4063bbc131962e85e5ea950a781bd31c2793d1
Kassem <systemry420@gmail.com>
2022-03-19 18:58:05 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
+
+import {
+  HomePage
+} from './pages'
+
+function App() {
+  return <h4>comfy sloth starter</h4>
+}
+
+export default App
